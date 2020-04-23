646317fd9c803efad12501a5e2b5c2dc55e14900
Luc Marechal <36209435+LucMarechal@users.noreply.github.com>
2020-04-23 17:40:10 +0200

    Add files via upload

---
+++ Dash_app/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Soft Robotics Materials Database',
+   version='1.0',
+   description='A useful module',
+   author='Luc Marechal',
+   author_email='',
+   packages=['Soft Robotics Materials Database'],  #same as name
+   #install_requires=['bar', 'greek'], #external packages as dependencies
+)
